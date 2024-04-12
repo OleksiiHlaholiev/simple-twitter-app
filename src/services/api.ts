e19b63b0f707eaea37a024527638bc082837ed97
oleksiiHlaholiev <oleksii.hlaholiev@magnise.com.ua>
2024-04-12 13:32:23 +0300

    code refactoring

--- src/services/api.ts
+++ src/services/api.ts
@@ -45,16 +45,6 @@ export const getPostById = (id = 0): Promise<IPost> => {
     return asyncRequest(requestURL);
 };
 
-/* providing token in bearer */
-/*fetch('https://dummyjson.com/auth/me', {
-    method: 'GET',
-    headers: {
-        'Authorization': 'Bearer /!* YOUR_TOKEN_HERE *!/',
-    },
-})
-    .then(res => res.json())
-    .then(console.log);*/
-
 export const getLoggedUserInfo = () => {
     const requestURL = `${URL_API_BASE}/auth/me`;
 
