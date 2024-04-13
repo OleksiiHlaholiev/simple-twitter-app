a8b2a55ea1687ef50eaec671afdeaea6b9016415
oleksiiHlaholiev <oleksii.hlaholiev@magnise.com.ua>
2024-04-13 23:13:45 +0300

    App: updated routing logic (via redux store)

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,40 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './App.scss';
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import {BrowserRouter, Navigate, Route, Routes} from 'react-router-dom';
 import Layout from './components/Layout';
 import LoginPage from './components/LoginPage';
 import HomePage from "./components/HomePage";
 import DetailPage from './components/DetailPage';
 import SettingsPage from "./components/SettingsPage";
-import { ROUTES_PATH } from './constants';
+import {ROUTES_PATH} from './constants';
+import {useTypedSelector} from "./hooks/useTypedSelector";
 
 const App = () => {
-    //const isAuth = localStorage.getItem('isAuth'); //TODO the logic
-    //const isAuth = true;
-    //const [isAuth, setIsAuth] = useState(false);
+    const {isLoggedIn} = useTypedSelector(state => state.user);
 
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Layout />}>
-                    <Route index element={<Navigate to={ROUTES_PATH.login} replace />} />
-                    <Route path={ROUTES_PATH.login} element={<LoginPage />} />
-                    <Route path={ROUTES_PATH.home} element={<HomePage />} />
-                    <Route path={`${ROUTES_PATH.detail}/:id`} element={<DetailPage />} />
-                    <Route path={ROUTES_PATH.settings} element={<SettingsPage />} />
-                    <Route path="*" element={<Navigate to={ROUTES_PATH.login} replace />} />
+                <Route path="/" element={<Layout/>}>
+                    <Route path={ROUTES_PATH.login} element={<LoginPage/>}/>
+
+                    {isLoggedIn ? (
+                        <>
+                            <Route index path={ROUTES_PATH.home} element={<HomePage/>}/>
+                            <Route path={`${ROUTES_PATH.detail}/:id`} element={<DetailPage/>}/>
+                            <Route path={ROUTES_PATH.settings} element={<SettingsPage/>}/>
+                        </>
+                    ) : (
+                        <>
+                            <Route index element={<Navigate to={ROUTES_PATH.login} replace/>}/>
+                        </>
+                    )}
+
+                    <Route path="*" element={<Navigate to={ROUTES_PATH.login} replace/>}/>
                 </Route>
             </Routes>
         </BrowserRouter>
     );
-
-    /*return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Layout />}>
-                    <Route path={ROUTES_PATH.login} element={<LoginPage />} />
-
-                    {isAuth ? (
-                        <>
-                            <Route index path={ROUTES_PATH.home} element={<HomePage />} />
-                            <Route path={`${ROUTES_PATH.detail}/:id`} element={<DetailPage />} />
-                            <Route path={ROUTES_PATH.settings} element={<SettingsPage />} />
-                        </>
-                    ): (
-                        <>
-                            <Route index element={<Navigate to={ROUTES_PATH.login} replace />} />
-                        </>
-                    )}
-
-                    <Route path="*" element={<Navigate to={ROUTES_PATH.login} replace />} />
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    );*/
 };
 
 export default App;
