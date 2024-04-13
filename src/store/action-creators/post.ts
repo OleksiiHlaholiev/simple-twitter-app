4e8bc7695fb3d796385a6454a27dd28fa7ebf7d5
oleksiiHlaholiev <oleksii.hlaholiev@magnise.com.ua>
2024-04-13 11:27:17 +0300

    updated redux store logic for posts (on HomePage)

--- src/store/action-creators/post.ts
+++ src/store/action-creators/post.ts
@@ -1,22 +1,27 @@
 import {PostAction, PostActionTypes} from "../../dataTypes/dataTypes";
 import {Dispatch} from "redux";
-import { asyncRequest } from "../../services/api";
+import {asyncRequest} from "../../services/api";
 import {POSTS_LIMIT, URL_API_BASE} from "../../constants";
 
-//TODO: the logic with store
-export const fetchPosts = (skip: number = 0) => {
+//TODO: check the logic with store
+export const fetchPosts = (page: number = 0) => {
     return async (dispatch: Dispatch<PostAction>) => {
         try {
             dispatch({type: PostActionTypes.FETCH_POSTS});
+            const skip = page * POSTS_LIMIT;
             const requestURL = `${URL_API_BASE}/posts?limit=${POSTS_LIMIT}&skip=${skip}`;
             const response = await asyncRequest(requestURL);
 
-            dispatch({type: PostActionTypes.FETCH_POSTS_SUCCESS, payload: response.posts});
-        } catch (e) {
+            dispatch({type: PostActionTypes.FETCH_POSTS_SUCCESS, payload: response});
+        } catch (error: any) {
             dispatch({
                 type: PostActionTypes.FETCH_POSTS_ERROR,
-                payload: 'Произошла ошибка при загрузке пользователей'
+                payload: error.message,
             })
         }
     }
+}
+
+export function setPostsPage(page: number): PostAction {
+    return {type: PostActionTypes.SET_POSTS_PAGE, payload: page}
 }
